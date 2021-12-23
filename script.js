93df6198072dcdf1b58e5196ca8d091362a5779e
jwp9633 <47207736+jwp9633@users.noreply.github.com>
2021-12-23 15:34:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function borderBottom(id, style) {
+    document.getElementById(id).style.borderBottom = style;
+}
+function border(id, style) {
+    document.getElementById(id).style.border = style;
+}
+function fontColor(id, color) {
+    document.getElementById(id).style.color = color;
+}
+function background_color(id, color) {
+    document.getElementById(id).style.backgroundColor = color;
+}
+function doDisplay(id, display) {
+    var con = document.getElementById(id);
+    con.style.display = display;
+}
